91498fbb13a888823570a9a6e6dd376f9d642cd4
Fhupsel <fmhupsel@outlook.com>
2023-11-23 12:39:28 -0300

    fix

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
 function obterValorDoInput() {
-  const logradouro = document.querySelector('.logradouro')
-  const bairro = document.querySelector('.bairro')
-  const localidade = document.querySelector('.localidade')
-  const uf = document.querySelector('.uf')
+  const logradouro = document.querySelector('.logradouro');
+  const bairro = document.querySelector('.bairro');
+  const localidade = document.querySelector('.localidade');
+  const uf = document.querySelector('.uf');
   const cep = document.getElementById('cep').value;
   const url = `https://viacep.com.br/ws/${cep}/json/`;
-
-  const endereço = fetch(url);
 
   logradouro.innerHTML = '';
   bairro.innerHTML = '';
   localidade.innerHTML = '';
   uf.innerHTML = '';
 
-  endereço.then(r => r.json())
-  .then(body => {
-    console.log(body);
-    logradouro.innerHTML += body.logradouro;
-    bairro.innerText += body.bairro;
-    localidade.innerText += body.localidade;
-    uf.innerText += body.uf;
+  fetch(url)
+    .then(response => response.json())
+    .then(body => {
 
-    inicializarMapa(cep);
-  })
+      logradouro.innerHTML += body.logradouro;
+      bairro.innerText += body.bairro;
+      localidade.innerText += body.localidade;
+      uf.innerText += body.uf;
+
+      return body;
+    })
+    .then(inicializarMapa)  
+    .catch(error => {
+      console.error('Ocorreu um erro:', error);
+    });
 }
 
-function inicializarMapa(cep) {
-  // Obtenha o CEP do usuário (pode ser obtido de um campo de entrada HTML, por exemplo)
-  // Use um serviço de geocodificação para obter as coordenadas a partir do CEP
+function inicializarMapa(dados) {
   var geocoder = new google.maps.Geocoder();
 
-  geocoder.geocode({ 'address': cep }, function (results, status) {
+  geocoder.geocode({ 'address': dados.cep }, function (results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
-      // Obtenha as coordenadas
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
 
-      // Crie um objeto de opções para o mapa
       var mapOptions = {
         center: new google.maps.LatLng(latitude, longitude),
-        zoom: 15, // Ajuste o nível de zoom conforme necessário
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
-      // Crie o mapa
       var mapa = new google.maps.Map(document.getElementById('mapa'), mapOptions);
 
-      // Adicione um marcador no mapa
       var marcador = new google.maps.Marker({
         position: new google.maps.LatLng(latitude, longitude),
         map: mapa,
